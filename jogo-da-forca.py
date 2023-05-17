8c2d80fe66bbf2170f4f02bc3610d09142593c0c
Giovanna Giselli Santana de Oliveira <113366850+jovuanna@users.noreply.github.com>
2023-05-17 19:33:17 -0300

    Add files via upload

--- jogo-da-forca.py
+++ jogo-da-forca.py
@@ -4,18 +4,35 @@ def intro(jogo):
     print('---------------------------')
 
 
-import random
-
-palavras = 'Julian'.upper()
 intro("Jogo da forca")
 '''sorteio = random.choices(palavras) #sorteia um elemento da lista'''
+palavras = 'Julian'.upper()
+
 acertos = 0
 erros = 0
 
-while acertos != len(palavras) or erros != 6:
+letras_acertadas = ''
+letras_erradas = ''
+
+while acertos != len(palavras) and erros != 5:
+    msg = ''
+    print(f'Você já acertou: ' + letras_acertadas)
+    print(f'Você já errou: ' + letras_erradas)
     letra = input("Digite uma letra: ").upper()
     if letra in palavras:
         print("Você acertou!")
+        letras_acertadas += letra
+        acertos += 1
     else:
         print("Você errou!")
+        letras_erradas += letra
+        acertos += 1
+    for letra in palavras:
+        if letra in letras_acertadas:
+            msg += letra
+
+        else:
+            msg += '_'
+    print(msg)
+
 '''print(f"A palavra era: {palavras}")'''
