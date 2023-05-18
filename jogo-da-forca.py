b4b6dd51becf52a9ac8e2862a7e989f1047cc8d8
Giovanna Giselli Santana de Oliveira <113366850+jovuanna@users.noreply.github.com>
2023-05-18 20:12:41 -0300

    Add files via upload

--- jogo-da-forca.py
+++ jogo-da-forca.py
@@ -1,12 +1,45 @@
 def intro(jogo):
     print('---------------------------')
-    print('-'*6, jogo,'-'*6)
+    print(' '*6, jogo, ' '*6)
     print('---------------------------')
 
 
+forca = '''
+_____
+    |
+    -'''
+vazio = '''
+'''
+
+cabeca = '''
+    O
+'''
+tronco = '''
+    O
+    |
+'''
+b_direito = '''
+    O
+   /|
+'''
+b_esquerdo = '''
+    O
+   /|\\
+'''
+p_direita = '''
+    O
+   /|\\
+   / \\
+'''
+p_esquerda = '''
+    O
+   /|\\
+   / \\
+'''
 intro("Jogo da forca")
-'''sorteio = random.choices(palavras) #sorteia um elemento da lista'''
-palavras = 'Julian'.upper()
+
+
+#print(forca+tronco)
 
 acertos = 0
 erros = 0
@@ -14,25 +47,34 @@ erros = 0
 letras_acertadas = ''
 letras_erradas = ''
 
+# sorteio = random.choices(palavras) #sorteia um elemento da lista
+palavras = 'Julian'.upper()
+
+
+
 while acertos != len(palavras) and erros != 5:
     msg = ''
-    print(f'Você já acertou: ' + letras_acertadas)
-    print(f'Você já errou: ' + letras_erradas)
-    letra = input("Digite uma letra: ").upper()
+    for letra in palavras:
+        if letra in letras_acertadas:
+            msg += letra + ' '  # mostra a letra no lugar + um espaco
+        else:
+            msg += '_'  # mostra que não tem nenhuma letra acertada ainda naquela posição
+    chances = [vazio, cabeca, tronco, b_direito, b_esquerdo, p_direita, p_esquerda]
+    print(forca + chances[erros])
+    print(msg)
+
+    print(f'Você já acertou: ' + letras_acertadas) # junta as letras acertadas
+
+    letra = input("Digite uma letra: ").upper() # o upper serve pra deixar a letra em caixa alta independente de como o usuário digitar
     if letra in palavras:
         print("Você acertou!")
-        letras_acertadas += letra
+        letras_acertadas += letra # mantém as a letras certas adicionadas no laço de repetição anterior
         acertos += 1
     else:
         print("Você errou!")
         letras_erradas += letra
         acertos += 1
-    for letra in palavras:
-        if letra in letras_acertadas:
-            msg += letra
 
-        else:
-            msg += '_'
-    print(msg)
+
 
 '''print(f"A palavra era: {palavras}")'''
